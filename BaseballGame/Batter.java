3ba7ed685f9a67ea7b6f8a7a8553133de1eadec0
DolanOats <777dlzo@gmail.com>
2019-05-23 13:24:04 -0700

    constructors

---
+++ BaseballGame/Batter.java
@@ -0,0 +1,12 @@
+public class Batter extends SingleInning
+{
+    private int hit;
+    private int pwr;
+    private int spd;
+    public Batter(int hit1, int pwr1, int spd1)
+    {
+        hit = hit1;
+        pwr = pwr1;
+        spd = spd1;
+    }
+}
